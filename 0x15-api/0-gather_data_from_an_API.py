0a8c9f94c006899c330a3aef7fc71a5b3d9d3f23
ela-hidri <elahidri.eh@gmail.com>
2023-10-23 12:28:53 -0700

    adding 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """  returns information about his/her TODO list progress. """
+import json
 import requests
 from sys import argv
-import json
 
 if __name__ == "__main__":
     if len(argv) > 1:
