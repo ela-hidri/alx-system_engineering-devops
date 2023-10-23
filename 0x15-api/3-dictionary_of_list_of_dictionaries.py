f463b2a8855576ab0673fb0eb2c1eb882e2d1f03
ela-hidri <elahidri.eh@gmail.com>
2023-10-23 13:47:53 -0700

    fixing code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,17 +5,21 @@ import json
 import requests
 from sys import argv
 
+
+def todos(id):
+    """ get todos """
+    url = 'https://jsonplaceholder.typicode.com/'
+    return requests.get(url + "todos", params={"userId": id}).json()
+
+
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     users = requests.get(url + "users").json()
     with open('todo_all_employees.json', "w") as outfile:
-        for u in users:
-            id = u.get('id')
-            todos = requests.get(url + "todos", params={"userId": id}).json()
-            json.dump({id: [{
-                      "username": u.get('username'),
-                      "task": t.get('title'),
-                      "completed": t.get('completed')
-                      }for t in todos]},
-                      outfile)
-    outfile.close()
+        json.dump({u.get("id"): [{
+                  "username": u.get("username"),
+                  "task": t.get("title"),
+                  "completed": t.get("completed")
+                  } for t in todos(u.get("id"))]
+                  for u in users}, outfile)
+    oufile.close()
