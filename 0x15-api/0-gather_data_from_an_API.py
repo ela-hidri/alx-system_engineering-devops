53234763fcb098ee1918616e28612bf08b902ca3
ela-hidri <elahidri.eh@gmail.com>
2023-10-23 12:26:43 -0700

    adding 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  returns information about his/her TODO list progress. """
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        url = 'https://jsonplaceholder.typicode.com/'
+        user = requests.get(url + "users/{}".format(argv[1])).json()
+        todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+        cmpl = [t.get('title') for t in todos if t.get('completed') is True]
+        print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+                                                              len(cmpl),
+                                                              len(todos)))
+        [print("\t {}".format(task)) for task in cmpl]
