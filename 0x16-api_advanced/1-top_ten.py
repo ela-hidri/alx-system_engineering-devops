15e479db61897ee40316625c45c8d0beda6e0c1a
ela-hidri <elahidri.eh@gmail.com>
2023-11-07 12:03:56 -0800

    adding files

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers """
+import requests
+
+
+def top_ten(subreddit):
+    """ get top 10 """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
